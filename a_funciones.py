7343308297d9b3be1861eaf163dca5337ac62629
auxiliar.vicedoce <pendiente.por.asignar@udea.edu.co>
2024-03-30 16:17:20 -0500

    Primer commite

---
+++ a_funciones.py
@@ -0,0 +1,6 @@
+def ejecutar_sql(nombre_archivo, cur):
+    sql_file = open(nombre_archivo)
+    sql_as_string = sql_file.read()
+    sql_file.close
+    cur.executescript(sql_as_string)
+    return cur.fetchall()
